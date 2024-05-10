e1f9efe2c0902362472f6f39990221896ec5ec8d
Luan Matos <luanmatos.ti@gmail.com>
2024-05-10 12:48:20 -0300

    finished project

--- src/main/java/br/com/projeto/jogo/adivinhacao/Main.java
+++ src/main/java/br/com/projeto/jogo/adivinhacao/Main.java
@@ -1,9 +1,34 @@
 package br.com.projeto.jogo.adivinhacao;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-        
+        jogoAdivinhacao();
+    }
 
+    public static void jogoAdivinhacao() {
+        Random random = new Random(); // Criação de Objeto Random
+        Scanner scanner = new Scanner(System.in); // Criação de Objeto Scanner usado para receber a entrada do usuário pelo console
+
+        int randomNumber = random.nextInt(21); // Gera um número aleatório entre 0 e 20
+        int chosenNumber;
+        int attempts = 0;
+
+        do {
+            attempts++;
+            System.out.println("Digite um número:");
+            chosenNumber = scanner.nextInt();
+
+            if (randomNumber > chosenNumber) {
+                System.out.println("O número " + chosenNumber + " é menor do que o número escolhido.");
+            } else if (randomNumber < chosenNumber) {
+                System.out.println("O número " + chosenNumber + " é maior do que o número escolhido.");
+            } else {
+                System.out.println("Parabéns, você acertou em " + attempts + " tentativa(s)!");
+            }
+            System.out.println();
+        } while (randomNumber != chosenNumber);
     }
 }
